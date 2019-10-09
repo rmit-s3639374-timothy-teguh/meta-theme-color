56e9e4f9b93aec4887493a59aa2bc9141cd0ff10
rmit-s3639374-timothy-teguh <s3639374@student.rmit.edu.au>
2019-10-09 15:05:43 +1100

    Initial comit for stats page

--- main.php
+++ main.php
@@ -8,7 +8,7 @@
 <head>
   <title>Meta Theme Color</title>
   <meta charset='UTF-8'>
-  <link rel='stylesheet' type='text/css' href='/css/main.css'>
+  <link rel='stylesheet' type='text/css' href='/css/default.css'>
 </head>
 
 <body>
